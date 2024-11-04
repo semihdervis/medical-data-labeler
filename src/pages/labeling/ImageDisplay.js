ae4a7cb5cc9d1ce212ea243bb02c90b75ea077e6
ozlemkrc <kurucaozlem@gmail.com>
2024-11-04 23:05:06 +0300

    images can be opened bigger

--- src/pages/labeling/ImageDisplay.js
+++ src/pages/labeling/ImageDisplay.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 import './ImageDisplay.css';
 
 function ImageDisplay({ image, onNextImage }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const displayImage = image || '/logo192.png';
 
-  //I took the save button from here and put it in LabelingInterFace.js
+  const handleImageClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div className="image-display">
-      <img src={displayImage} alt="Patient Medical" />
+      <img src={displayImage} alt="Patient Medical" onClick={handleImageClick} />
       <button onClick={onNextImage} className="next-image-button">
         Next Image
       </button>
+
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={closeModal}>
+          <div className="modal-content">
+            <img src={displayImage} alt="Enlarged View" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
