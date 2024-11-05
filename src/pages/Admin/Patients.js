dcdc7c5ee5cd3f62a1e7ced1894eb4d29d8235aa
ozlemkrc <kurucaozlem@gmail.com>
2024-11-05 20:45:27 +0300

    import from file button

--- src/pages/Admin/Patients.js
+++ src/pages/Admin/Patients.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import removeIcon from '../icons/remove.png';
+import fileIcon from '../icons/file.png';
 
 function Patients({ patients, setPatients }) {
+  const fileInputRef = useRef(null);
+
   const handleAddPatient = () => {
     setPatients([...patients, { id: `Patient${patients.length + 1}`, images: [] }]);
+  };
+
+  const handleImportPatients = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const importedPatients = JSON.parse(e.target.result);
+        setPatients([...patients, ...importedPatients]);
+      };
+      reader.readAsText(file);
+    }
   };
 
   return (
@@ -20,12 +35,25 @@ function Patients({ patients, setPatients }) {
             }}
           >
             <img src={removeIcon} alt="Remove" style={{ width: '20px', height: '20px' }} />
-            </button>
+          </button>
         </div>
       ))}
       <button className="in-page-buttons" onClick={handleAddPatient}>
         Add Patient
       </button>
+      
+      {/* Hidden file input and associated label for "Import Patients from File" */}
+      <input
+        type="file"
+        ref={fileInputRef}
+        className="import-patients"
+        accept=".json"
+        onChange={handleImportPatients}
+      />
+      <label onClick={() => fileInputRef.current.click()} className="import-patients-label">
+        Import Patients
+        <img src={fileIcon} alt="Import" style={{ width: '15px', height: '15px' }} />
+      </label>
     </section>
   );
 }
