bf99c21379d47cac5f5658422ac6fd9039c22e7b
semihdervis <121356726+semihdervis@users.noreply.github.com>
2024-11-29 15:56:07 +0300

    merge labeling interface and patient list sidebar

--- src/pages/labeling/LabelingInterface.js
+++ src/pages/labeling/LabelingInterface.js
@@ -1,12 +1,12 @@
-import { useNavigate } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-import './LabelingInterface.css';
-import PatientListSidebar from './PatientListSidebar';
-import PatientInfoSidebar from './PatientInfoSidebar';
-import ImageDisplay from './ImageDisplay';
-import ImageLabelsSidebar from './ImageLabelsSidebar';
-import backArrow from '../icons/back_arrow.png';
-import saveIcon from '../icons/save.png';
+import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import "./LabelingInterface.css";
+import PatientInfoSidebar from "./PatientInfoSidebar";
+import ImageDisplay from "./ImageDisplay";
+import ImageLabelsSidebar from "./ImageLabelsSidebar";
+import backArrow from "../icons/back_arrow.png";
+import saveIcon from "../icons/save.png";
+import sorticon from "../icons/sort_icon.png";
 
 function LabelingInterface() {
   const navigate = useNavigate();
@@ -18,22 +18,22 @@ function LabelingInterface() {
 
   const patients = [
     {
-      id: 'Patient001',
-      name: 'John Doe',
+      id: "Patient001",
+      name: "John Doe",
       age: 45,
-      gender: 'Male',
-      healthCondition: 'Hypertensive',
-      overallCondition: 'Signs of Inflammation',
-      images: ['/behcet-hastaligi4.png', 'behcet_img3.jpg']
+      gender: "Male",
+      healthCondition: "Hypertensive",
+      overallCondition: "Signs of Inflammation",
+      images: ["/behcet-hastaligi4.png", "behcet_img3.jpg"],
     },
     {
-      id: 'Patient002',
-      name: 'Jane Smith',
+      id: "Patient002",
+      name: "Jane Smith",
       age: 34,
-      gender: 'Female',
-      healthCondition: 'Healthy',
-      overallCondition: 'Good',
-      images: ['/behcet_img.jpg', 'behcet_img2.png']
+      gender: "Female",
+      healthCondition: "Healthy",
+      overallCondition: "Good",
+      images: ["/behcet_img.jpg", "behcet_img2.png"],
     },
     // Add more patients here
   ];
@@ -62,7 +62,9 @@ function LabelingInterface() {
 
   const handlePreviousImage = () => {
     const currentIndex = selectedPatient.images.indexOf(selectedImage);
-    const previousIndex = (currentIndex - 1 + selectedPatient.images.length) % selectedPatient.images.length;
+    const previousIndex =
+      (currentIndex - 1 + selectedPatient.images.length) %
+      selectedPatient.images.length;
     setSelectedImage(selectedPatient.images[previousIndex]);
   };
 
@@ -70,36 +72,126 @@ function LabelingInterface() {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+  {/* PatientListSidebar functions */}
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc"); // Default sort order
+  const [showSortOptions, setShowSortOptions] = useState(false); // State for showing sort options
+
+  // Filter patients based on the search term
+  const filteredPatients = patients.filter((patient) =>
+    patient.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  // Sort patients based on ID
+  const sortedPatients = filteredPatients.sort((a, b) => {
+    return sortOrder === "asc"
+      ? a.id.localeCompare(b.id)
+      : b.id.localeCompare(a.id);
+  });
+
+  const handleClick = (patient) => {
+    console.log("Patient selected:", patient); // Log selected patient object
+    handleSelectPatient(patient);
+  };
+
+  {/* End PatientListSidebar functions */}
+
   return (
-    <div className={`labeling-interface ${isSidebarOpen ? 'sidebar-open' : ''}`}>
-      <div className='label-top-bar'>
-        <button className="sidebar-toggle-button" onClick={toggleSidebar}> 
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3 6h18v2H3zm0 5h18v2H3zm0 5h18v2H3z"/></svg>
+    <div
+      className={`labeling-interface ${isSidebarOpen ? "sidebar-open" : ""}`}
+    >
+      <div className="label-top-bar">
+        <button className="sidebar-toggle-button" onClick={toggleSidebar}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M3 6h18v2H3zm0 5h18v2H3zm0 5h18v2H3z" />
+          </svg>
         </button>
-        <div className='container'>
-          <button className="dashboard-button" onClick={() => navigate('/dashboard')}>
-          <img src={backArrow} alt="Back Arrow" style={{ width: '20px', height: '20px' }} />
-             Back to Dashboard</button>
+        <div className="container">
+          <button
+            className="dashboard-button"
+            onClick={() => navigate("/dashboard")}
+          >
+            <img
+              src={backArrow}
+              alt="Back Arrow"
+              style={{ width: "20px", height: "20px" }}
+            />
+            Back to Dashboard
+          </button>
           <button className="save-button" onClick={handleSave}>
-          <img src={saveIcon} alt="Save" style={{ width: '20px', height: '20px' }} />
-            Save</button>
+            <img
+              src={saveIcon}
+              alt="Save"
+              style={{ width: "20px", height: "20px" }}
+            />
+            Save
+          </button>
         </div>
       </div>
-      
-      <PatientListSidebar 
-        isOpen={isSidebarOpen} 
-        onSelectPatient={handleSelectPatient} 
-        patients={patients} // Pass the patients data to the PatientListSidebar
-      />
-      
+      {/* Patient List Sidebar*/}
+      <div className="patient-list-sidebar">
+        <h3>Patients</h3>
+
+        <div className="search-sort-container">
+          <input
+            type="text"
+            placeholder="Search patients..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <div className="sort-container">
+            <button
+              className="sortbutton"
+              onClick={() => setShowSortOptions((prev) => !prev)}
+            >
+              <img
+                src={sorticon}
+                alt="Sort"
+                style={{ width: "20px", height: "20px" }}
+              />
+            </button>
+            {showSortOptions && (
+              <div className="sort-options">
+                <button
+                  onClick={() => {
+                    setSortOrder("asc");
+                    setShowSortOptions(false);
+                  }}
+                >
+                  Ascending ID
+                </button>
+                <button
+                  onClick={() => {
+                    setSortOrder("desc");
+                    setShowSortOptions(false);
+                  }}
+                >
+                  Descending ID
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+
+        <ul>
+          {sortedPatients.map((patient) => (
+            <li key={patient.id} onClick={() => handleClick(patient)}>
+              {patient.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* End Patient List Sidebar*/}
+
       {/* Pass the selected patient to PatientInfoSidebar */}
       <PatientInfoSidebar patient={selectedPatient} />
 
       {/* Pass the selected image to ImageDisplay */}
-      <ImageDisplay 
-        image={selectedImage} 
-        onNextImage={handleNextImage} 
-        onPreviousImage={handlePreviousImage} 
+      <ImageDisplay
+        image={selectedImage}
+        onNextImage={handleNextImage}
+        onPreviousImage={handlePreviousImage}
       />
       <ImageLabelsSidebar />
     </div>
