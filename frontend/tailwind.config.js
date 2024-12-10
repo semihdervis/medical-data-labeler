aceb8a37179969833a224de10b45c5f67486bdf3
ozlemkrc <kurucaozlem@gmail.com>
2024-12-10 21:43:47 +0300

    unnecessary colors deleted

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,15 +9,8 @@ export default {
       colors: {
         primary: "#3f51b5",
         secondary: "#303f9f",
-        lightblue: "#5c6bc0",
-        hoverblue: "#0f1b65",
         negative: "#d32f2f",
-        authbutton: "#1976d2",
-        authbuttonhover: "#1565c0",
       },
-      boxShadow:{
-        'custom-negative': '0 4px 6px rgba(211, 47, 47, 0.5)',
-      }
     },
   },
   plugins: [],
